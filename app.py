77690c375326cad5b212505925839b4ea9bd671c
suttondaniel <dansut13@gmail.com>
2020-09-20 20:20:08 -0700

    simplified stuff

--- app.py
+++ app.py
@@ -11,25 +11,17 @@ heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 class Dataentry(db.Model):
-    __tablename__ = "dataentry"
     id = db.Column(db.Integer, primary_key=True)
     mydata = db.Column(db.Text())
 
-    def __init__(self, mydata):
-        self.mydata = mydata
-
 @app.route("/submit", methods=["POST"])
 def post_to_db():
-    indata = Dataentry(request.form['mydata'])
-    data = copy(indata. __dict__ )
-    del data["_sa_instance_state"]
-    try:
-        db.session.add(indata)
-        db.session.commit()
-    except Exception as e:
-        print("\n FAILED entry: {}\n".format(json.dumps(data)))
-        print(e)
-        sys.stdout.flush()
+    items = request.form['mydata']
+
+    newitem = Dataentry(mydata=items)
+    db.session.add(newitem)
+    db.session.commit()
+
     return 'Success! To enter more data, <a href="{}">click here!</a>'.format(url_for("enter_data"))
 
 @app.route("/")
